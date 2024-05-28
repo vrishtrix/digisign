1950b499504e534d0373c339ebbb4b51b3f6acc4
Vrishin Patel <vrishin_patel@outlook.com>
2024-05-28 12:49:21 +0530

    chore: add essential middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono';
+import { compress } from 'hono/compress';
+import { csrf } from 'hono/csrf';
+import { prettyJSON } from 'hono/pretty-json';
+import { timeout } from 'hono/timeout';
 
 import apiRoutes from '@/api';
 
 const app = new Hono();
 
+// Registering app-level middlewares
+app.use(prettyJSON({ space: 4 }));
+app.use('/api', timeout(10 * 1000));
+app.use(compress({ encoding: 'gzip' }));
+app.use(
+	csrf({
+		origin: process.env.CSRF_ORIGIN ?? 'localhost',
+	}),
+);
+
+// Registering routes
 app.route('/api', apiRoutes);
 
 app.get('/', (c) => {
