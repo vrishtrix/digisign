ef7b2a2cb0a9b9fdf67ddd8f429ec5b7cf2e9c9a
Vrishin Patel <vrishin_patel@outlook.com>
2024-05-28 15:20:31 +0530

    chore: create types for request parameters

---
+++ src/types.d.ts
@@ -0,0 +1,32 @@
+type FileType = '1' | '2' | '3';
+
+export type SignupRequestParams = {
+	email: string;
+	password: string;
+	confirmPassword: string;
+	firstName: string;
+	lastName: string;
+};
+
+export type LoginRequestParams = {
+	email: string;
+	password: string;
+};
+
+export type PfxUploadRequestParams = {
+	file: File;
+	fileType: FileType;
+};
+
+export type SignPdfRequestParams = {
+	file: File;
+	fileType: FileType;
+	signLocation?: string;
+	signReason?: string;
+	pointX: string;
+	pointY: string;
+	signHeight: string;
+	signWidth: string;
+	pfxId: string;
+	signOnPage: 'F' | 'L' | 'A';
+};
